74be58874701ca464a983af26ce3f7547acbd987
Dima Gutierrez <117530171+DimaGutierrez@users.noreply.github.com>
2022-11-20 21:38:13 -0300

    Add files via upload

--- Formulario.java
+++ Formulario.java
@@ -1,38 +1,26 @@
 import javax.swing.*;
-import java.awt.event.*;
 
-public class Formulario extends JFrame implements ActionListener{
+public class Formulario extends JFrame{
 
  private JTextField textfield1;
- private JLabel label1;
- private JButton boton1;
+ private JScrollPane scrollpane1;
+ private JTextArea textarea1;
 
- public Formulario (){
-  setLayout(null);  
-  label1 = new JLabel("Usuario");
-  label1.setBounds(10,10,100,30);
-  add(label1);
-
+ public Formulario(){
+  setLayout(null);
   textfield1 = new JTextField();
-  textfield1.setBounds(120,17,150,20);
+  textfield1.setBounds(10,10,200,30);
   add(textfield1);
 
-  boton1 = new JButton("Aceptar");
-  boton1.setBounds(10,80,100,30);
-  add(boton1);
-  boton1.addActionListener(this);
+  textarea1 = new JTextArea();
+  scrollpane1 = new JScrollPane(textarea1);
+  scrollpane1.setBounds(10,50,400,300);
+  add(scrollpane1);
  }
-  
- public void actionPerformed(ActionEvent e){
-  if(e.getSource() == boton1){
-   String texto = textfield1.getText();
-   setTitle(texto);
-  }
- }
-
+ 
  public static void main(String args[]){
   Formulario formulario1 = new Formulario();
-  formulario1.setBounds(0,0,300,150);
+  formulario1.setBounds(0,0,540,400);
   formulario1.setVisible(true);
   formulario1.setResizable(false);
   formulario1.setLocationRelativeTo(null);
